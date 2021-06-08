e5a41adb73fa811b7ec82615b7641dcbd6870bbb
tabishakhtar01 <tabishakhtar01@gmail.com>
2021-06-08 23:44:06 +0530

    Added has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
     validates :first_name, presence: true, length: { maximum: 50 }
@@ -6,6 +7,8 @@ class User < ApplicationRecord
     validates :email, presence: true, 
                     uniqueness: true, 
                     format: { with: VALID_EMAIL_REGEX }
+    validates :password, length: { minimum: 6 }
+    validates :password_confirmation, presence: true, on: :create
 
     before_save :to_lowercase
 
