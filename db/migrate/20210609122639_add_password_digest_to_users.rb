d02541d74c8bb33c54e1a16f5bd857b361b60569
tabishakhtar01 <tabishakhtar01@gmail.com>
2021-06-09 18:05:45 +0530

    Added Role to User

--- db/migrate/20210609122639_add_password_digest_to_users.rb
+++ db/migrate/20210609122639_add_password_digest_to_users.rb
@@ -1,4 +1,4 @@
-class AddPasswordDigestToUser < ActiveRecord::Migration[6.1]
+class AddPasswordDigestToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :password_digest, :string, null: false
   end
