f3f9fe14a674b2e3eb814dcf0b76f5b8abbab8c0
tabishakhtar01 <tabishakhtar01@gmail.com>
2021-06-11 02:01:37 +0530

    Added add question button and ui changes

--- app/javascript/src/components/Quizzes/ShowQuiz.jsx
+++ app/javascript/src/components/Quizzes/ShowQuiz.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 import Container from "components/Container";
 import PageLoader from "components/PageLoader";
 import quizzesApi from "apis/quizzes";
@@ -15,7 +15,7 @@ const ShowQuiz = () => {
       const response = await quizzesApi.show(id);
       setQuizDetails(response.data.quiz);
     } catch (error) {
-      //   logger.error(error);
+      alert(error);
     } finally {
       setPageLoading(false);
     }
@@ -31,8 +31,22 @@ const ShowQuiz = () => {
 
   return (
     <Container>
-      <h1 className="pb-3 pl-3 mt-3 mb-3 text-lg leading-5 text-bb-gray">
-        {quizDetails?.title}
+      <div className="flex justify-end">
+        <button
+          className="relative  px-4 py-2
+        text-sm font-medium leading-5 text-white transition duration-150
+         ease-in-out bg-bb-purple border border-transparent rounded-md
+         group hover:bg-opacity-90 focus:outline-none"
+        >
+          <Link to="/">Add questions</Link>
+        </button>
+      </div>
+      <h1 className="mt-10 text-6xl text-gray-600">{quizDetails?.title}</h1>
+      <h1
+        className="text-xl flex justify-center items-center"
+        style={{ height: "50vh" }}
+      >
+        There are no questions in this quiz.
       </h1>
     </Container>
   );
