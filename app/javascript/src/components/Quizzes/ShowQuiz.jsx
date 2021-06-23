2050c0350e4d6baf2b76bc2792aadf27953785c9
tabishakhtar01 <tabishakhtar01@gmail.com>
2021-06-23 17:19:20 +0530

    updated the public url link

--- app/javascript/src/components/Quizzes/ShowQuiz.jsx
+++ app/javascript/src/components/Quizzes/ShowQuiz.jsx
@@ -85,7 +85,7 @@ const ShowQuiz = ({ history }) => {
           <h1>
             Published, your public link is -{" "}
             <span className="underline text-blue-600">
-              http://localhost:3000/public/{slugData}
+              https://quizzy-rails.herokuapp.com/{slugData}
             </span>
           </h1>
         )}
